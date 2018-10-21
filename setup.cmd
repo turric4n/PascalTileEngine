aeb9e96c5cf46bea3c0e3cd4d7443606ac983fc9
Turrican <djkike@djkike.es>
2018-10-21 12:18:10 +0200

    Updated setup.cmd

--- setup.cmd
+++ setup.cmd
@@ -1,12 +1,17 @@
 @echo off
-if exist .git (
-  echo initializing dependancies
-  call initsubmodules.cmd
-  call src/deps/makelibtilengine.cmd
+echo ##############################
+echo # Native library dependancy  #
+echo ##############################
+echo do you want to compile native library or use a precompiled one?
+CHOICE /C ab /M "Build yourself or download manually from https://megamarc.itch.io/tilengine"
+if %errorlevel% == 1 (
+  goto :buildlibrary
 )
-if %libraryfail% == 1 (
-  echo Cannot compile Tilengine library automatically... You need Visual Studio Community 2017 in the default path. Don't worry you can compile libary manually with VC++ or GCC32
-)  
+if %errorlevel% == 2 (
+  goto :buildpascal
+) 
+
+:buildpascal
 echo ##############################
 echo # Pascal Tile Engine Wrapper #
 echo ##############################
@@ -18,4 +23,17 @@ if %errorlevel% == 1 (
 if %errorlevel% == 2 (
   build-windows-fpc.cmd
 ) 
+goto :eof
 
+:buildlibrary  
+echo you select build library using VC++ automatically, you need Visual Studio Community 2017 in the default path
+echo initializing dependancies
+call initsubmodules.cmd  
+call src/deps/makelibtilengine.cmd
+if %libraryfail% == 1 (
+  echo Cannot compile Tilengine library automatically... You need Visual Studio Community 2017 in the default path. Don't worry you can compile libary manually with VC++ or GCC32
+)
+goto :buildpascal
+
+:eof
+echo acaba
