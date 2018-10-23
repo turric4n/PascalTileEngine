12c3b6de430b3042291427276ee1699c4a215c56
Turrican <djkike@djkike.es>
2018-10-24 00:54:25 +0200

    More work and fixes

--- prebuilt/copylibs.cmd
+++ prebuilt/copylibs.cmd
@@ -11,17 +11,19 @@ if exist prebuilt\Tilengine\i386-linux\libTilengine.so_ move prebuilt\Tilengine\
 xcopy prebuilt\Tilengine\i386-linux\libTilengine.so "bin\package\i386-linux\"
 if exist prebuilt\Tilengine\i386-win32\Tilengine.dll_ move prebuilt\Tilengine\i386-win32\Tilengine.dll_ prebuilt\Tilengine\i386-win32\Tilengine.dll
 xcopy prebuilt\Tilengine\i386-win32\Tilengine.dll "bin\package\i386-win32\"
-if exist prebuilt\Tilengine\x86-64-win64\Tilengine.dll_ move prebuilt\Tilengine\x86-64-win64\Tilengine.dll_ prebuilt\Tilengine\x86-64-win64\Tilengine.dll
-xcopy prebuilt\Tilengine\x86-64-win64\Tilengine.dll "bin\package\x86-64-win64\"
-if exist prebuilt\Tilengine\x86-x64-darwin\Tilengine.dylib_ move prebuilt\Tilengine\x86-x64-darwin\Tilengine.dylib_ prebuilt\Tilengine\x86-x64-darwin\Tilengine.dylib
-xcopy prebuilt\Tilengine\x86-x64-darwin\Tilengine.dylib "bin\package\x86-x64-darwin\"
+if exist prebuilt\Tilengine\x86_64-win64\Tilengine.dll_ move prebuilt\Tilengine\x86_64-win64\Tilengine.dll_ prebuilt\Tilengine\x86_64-win64\Tilengine.dll
+xcopy prebuilt\Tilengine\x86_64-win64\Tilengine.dll "bin\package\x86_64-win64\"
+if exist prebuilt\Tilengine\x86_64-linux\libTilengine.so_ move prebuilt\Tilengine\x86_64-linux\libTilengine.so_ prebuilt\Tilengine\x86_64-linux\libTilengine.so
+xcopy prebuilt\Tilengine\x86_64-linux\libTilengine.so "bin\package\x86_64-linux\"
+if exist prebuilt\Tilengine\x86_x64-darwin\Tilengine.dylib_ move prebuilt\Tilengine\x86_x64-darwin\Tilengine.dylib_ prebuilt\Tilengine\x86_x64-darwin\Tilengine.dylib
+xcopy prebuilt\Tilengine\x86_x64-darwin\Tilengine.dylib "bin\package\x86_x64-darwin\"
 echo --------------------------------------------------
 echo ---Copying SDL2 native libraries (Only Windows)---
 echo --------------------------------------------------
 if exist prebuilt\SDL2\i386-win32\SDL2.dll_ move prebuilt\SDL2\i386-win32\SDL2.dll_ prebuilt\SDL2\i386-win32\SDL2.dll
 xcopy prebuilt\SDL2\i386-win32\SDL2.dll "bin\package\i386-win32\"
-if exist prebuilt\SDL2\x86-64-win64\SDL2.dll_ move prebuilt\SDL2\x86-64-win64\SDL2.dll_ prebuilt\SDL2\x86-64-win64\SDL2.dll
-xcopy prebuilt\SDL2\x86-64-win64\SDL2.dll "bin\package\x86-64-win64\"
+if exist prebuilt\SDL2\x86_64-win64\SDL2.dll_ move prebuilt\SDL2\x86_64-win64\SDL2.dll_ prebuilt\SDL2\x86_64-win64\SDL2.dll
+xcopy prebuilt\SDL2\x86_64-win64\SDL2.dll "bin\package\x86_64-win64\"
 
 
 
--- prebuilt/gitignorelibs.cmd
+++ prebuilt/gitignorelibs.cmd
@@ -4,10 +4,11 @@ if exist prebuilt\Tilengine\arm-linux\libTilengine.so move prebuilt\Tilengine\ar
 if exist prebuilt\Tilengine\arm-linux\libTilengine.so move prebuilt\Tilengine\arm-linux\libTilengine.so prebuilt\Tilengine\arm-linux\libTilengine.so_
 if exist prebuilt\Tilengine\i386-linux\libTilengine.so move prebuilt\Tilengine\i386-linux\libTilengine.so prebuilt\Tilengine\i386-linux\libTilengine.so_
 if exist prebuilt\Tilengine\i386-win32\Tilengine.dll move prebuilt\Tilengine\i386-win32\Tilengine.dll prebuilt\Tilengine\i386-win32\Tilengine.dll_
-if exist prebuilt\Tilengine\x86-64-win64\Tilengine.dll move prebuilt\Tilengine\x86-64-win64\Tilengine.dll_ prebuilt\Tilengine\x86-64-win64\Tilengine.dll_
-if exist prebuilt\Tilengine\x86-x64-darwin\Tilengine.dylib move prebuilt\Tilengine\x86-x64-darwin\Tilengine.dylib prebuilt\Tilengine\x86-x64-darwin\Tilengine.dylib_
+if exist prebuilt\Tilengine\x86_64-win64\Tilengine.dll move prebuilt\Tilengine\x86_64-win64\Tilengine.dll prebuilt\Tilengine\x86_64-win64\Tilengine.dll_
+if exist prebuilt\Tilengine\x86_64-linux\libTilengine.so move prebuilt\Tilengine\x86_64-linux\libTilengine.so prebuilt\Tilengine\x86_64-linux\libTilengine.so_
+if exist prebuilt\Tilengine\x86_x64-darwin\Tilengine.dylib move prebuilt\Tilengine\x86_x64-darwin\Tilengine.dylib prebuilt\Tilengine\x86_x64-darwin\Tilengine.dylib_
 if exist prebuilt\SDL2\i386-win32\SDL2.dll move prebuilt\SDL2\i386-win32\SDL2.dll prebuilt\SDL2\i386-win32\SDL2.dll_
-if exist prebuilt\SDL2\x86-64-win64\SDL2.dll move prebuilt\SDL2\x86-64-win64\SDL2.dll prebuilt\SDL2\x86-64-win64\SDL2.dll_
+if exist prebuilt\SDL2\x86_64-win64\SDL2.dll move prebuilt\SDL2\x86_64-win64\SDL2.dll prebuilt\SDL2\x86_64-win64\SDL2.dll_
 
 
 
